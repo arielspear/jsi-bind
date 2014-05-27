d4b3305222a121e2806b5e502b2218c3c54c3dcc
Sam Averett <averettsam@comcast.net>
2014-05-27 13:37:51 -0700

    add mocha and chai

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var partialRight = require('../index').partialRight;
+
+describe('partialRight()', function() {
+	it('should apply arguments right to left', function() {
+		var greet = function(greeting, name) { return greeting + ' ' + name; };
+		var greetFred = partialRight(greet, 'fred');
+		expect(greetFred('hi')).to.eql('hi fred');
+	});
+});

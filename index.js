46b85b1f0596592a2e0cdeecf9038385c3d2d03b
Ariel Spear <simpleredfox@gmail.com>
2014-05-27 13:50:56 -0700

    failing test that is the reverse of what we want

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-module.exports.partialRight = function() {
-	
+module.exports.partialRight = function(fn) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	return function(){
+		var remainingArgs = Array.prototype.slice.call(arguments, 0);
+		var completedArgs = args.concat(remainingArgs);
+		return fn.apply(this, completedArgs);
+	};	
 };
 
 var partial = function(fn){

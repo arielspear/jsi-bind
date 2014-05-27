542b27316869bbae915423951ab583e1787185cd
Ariel Spear <simpleredfox@gmail.com>
2014-05-27 14:09:16 -0700

    Got partialRight to work, by changing what gets concatinated to what

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ module.exports.partialRight = function(fn) {
 	var args = Array.prototype.slice.call(arguments, 1);
 	return function(){
 		var remainingArgs = Array.prototype.slice.call(arguments, 0);
-		var completedArgs = args.concat(remainingArgs);
+		var completedArgs = remainingArgs.concat(args);
 		return fn.apply(this, completedArgs);
 	};	
 };
 
-var partial = function(fn){
-	var args = Array.prototype.slice.call(arguments, 1);
-	return function(){
-		var remainingArgs = Array.prototype.slice.call(arguments, 0);
-		var completedArgs = args.concat(remainingArgs);
-		return fn.apply(this, completedArgs);
-	};
-};
+// var partial = function(fn){
+// 	var args = Array.prototype.slice.call(arguments, 1);
+// 	return function(){
+// 		var remainingArgs = Array.prototype.slice.call(arguments, 0);
+// 		var completedArgs = args.concat(remainingArgs);
+// 		return fn.apply(this, completedArgs);
+// 	};
+//};

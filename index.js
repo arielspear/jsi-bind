7c8696be22523ce9fecfe5a6617d75f55333b178
Ariel Spear <simpleredfox@gmail.com>
2014-05-27 13:47:38 -0700

    Wrote in partial function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 module.exports.partialRight = function() {
 	
 };
+
+var partial = function(fn){
+	var args = Array.prototype.slice.call(arguments, 1);
+	return function(){
+		var remainingArgs = Array.prototype.slice.call(arguments, 0);
+		var completedArgs = args.concat(remainingArgs);
+		return fn.apply(this, completedArgs);
+	};
+};
